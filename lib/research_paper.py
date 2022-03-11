2c0c5510b4c47ed4a8d9937a46554983cfdcfc56
TonHai1111 <tonthatdaihai@gmail.com>
2022-03-11 11:19:22 -0600

    add year, volume, page

--- lib/research_paper.py
+++ lib/research_paper.py
@@ -10,21 +10,34 @@ from lib.variables import (
 
 class Research_Paper:
     def __init__(self):
-        self.host = CROSSREF_ORG_API
-        self.all_titles_jsons = {}
+        self.__host = CROSSREF_ORG_API
+        self.__all_titles_jsons = {}
+        self.__limit_rows = 5
+
+    def set_limit_rows(self, value):
+        self.__limit_rows = value
 
     def get_doi_from_title(self, title):
         data = self.get_json_from_title(title)
         try:
             doi = str(data["message"]["items"][0]["DOI"])
-        except KeyError:
+        except KeyError as k:
             doi = ""
-        except TypeError:
+            print("KeyError: " + str(k))
+        except TypeError as t:
             doi = ""
+            print("TypeError: " + str(t))
         return doi
 
     def get_json_from_title(self, title):
-        url = self.host + SEARCH_ON_TITLE + "=" + str(title)
+        url = (
+            self.__host
+            + SEARCH_ON_TITLE
+            + "="
+            + str(title)
+            + "&rows="
+            + str(self.__limit_rows)
+        )
         result = rq.get(url).text
         data = json.loads(result)
         return data
@@ -32,33 +45,97 @@ class Research_Paper:
     def get_doi_from_json(self, json_data):
         try:
             doi = str(json_data["message"]["items"][0]["DOI"])
-        except KeyError:
+        except KeyError as k:
             doi = ""
-        except TypeError:
+            print("KeyError: " + str(k))
+        except TypeError as t:
             doi = ""
+            print("TypeError: " + str(t))
         return doi
 
     def get_authors_from_json(self, json_data):
         try:
-            authors = str(json_data["message"]["items"][0]["DOI"])
-        except KeyError:
-            authors = ""
-        except TypeError:
-            authors = ""
+            authors = json_data["message"]["items"][0]["author"]
+        except KeyError as k:
+            authors = []
+            print("KeyError: " + str(k))
+        except TypeError as t:
+            authors = []
+            print("TypeError: " + str(t))
         return authors
+
+    def get_str_authors_from_json(self, json_data):
+        output = ""
+        array_authors = self.get_authors_from_json(json_data)
+        for i in range(len(array_authors)):
+            author = array_authors[i]
+            if output != "":
+                output += "; "
+            output += self.__extract_author_name(author)
+        return output
 
     def get_year_from_json(self, json_data):
         try:
-            year = str(json_data["message"]["items"][0]["DOI"])
-        except KeyError:
+            year = str(
+                json_data["message"]["items"][0]["published-print"]["date-parts"][0][0]
+            )
+        except KeyError as k:
             year = ""
-        except TypeError:
+            print("KeyError: " + str(k))
+        except TypeError as t:
             year = ""
+            print("TypeError: " + str(t))
         return year
 
+    def get_title_from_json(self, json_data):
+        try:
+            title = str(json_data["message"]["items"][0]["title"][0])
+        except KeyError as k:
+            title = ""
+            print("KeyError: " + str(k))
+        except TypeError as t:
+            title = ""
+            print("TypeError: " + str(t))
+        return title
+
+    def get_volume_from_json(self, json_data):
+        try:
+            volume = str(json_data["message"]["items"][0]["volume"])
+        except KeyError as k:
+            volume = ""
+            print("KeyError: " + str(k))
+        except TypeError as t:
+            volume = ""
+            print("TypeError: " + str(t))
+        return volume
+
+    def get_page_from_json(self, json_data):
+        try:
+            page = str(json_data["message"]["items"][0]["page"])
+        except KeyError as k:
+            page = ""
+            print("KeyError: " + str(k))
+        except TypeError as t:
+            page = ""
+            print("TypeError: " + str(t))
+        return page
+
     def add_title_json(self, title, data_json):
-        self.all_titles_jsons[title] = data_json
+        self.__all_titles_jsons[title] = data_json
 
     def dump_titles_jsons_file(self, filename=TITLE_JSON_OUTPUT):
         with open(filename, "w") as file:
-            json.dump(self.all_titles_jsons, file)
+            json.dump(self.__all_titles_jsons, file)
+
+    # Private methods
+    def __extract_author_name(self, author):
+        author_name = ""
+        if "family" in author:
+            author_name += str(author["family"])
+        if "given" in author:
+            if author_name != "":
+                author_name += ", "
+            author_name += str(author["given"])
+        if "name" in author:
+            author_name = str(author["name"])
+        return author_name
--- main.py
+++ main.py
@@ -40,10 +40,51 @@ def add_doi_json(csv_file, output_file):
     return
 
 
+def add_doi_authors_year_volume_page(csv_file, output_file):
+    all_rows = []
+    header = []
+    count = 0
+    with open(csv_file, "r") as file:
+        csvreader = csv.reader(file)
+        header = next(csvreader)  # header
+        for csv_row in csvreader:
+            title = process_special_character(str(csv_row[2]))
+            json_data = research_paper.get_json_from_title(title)
+            doi = research_paper.get_doi_from_json(json_data)
+            str_authors = research_paper.get_str_authors_from_json(json_data)
+            year = research_paper.get_year_from_json(json_data)
+            volume = research_paper.get_volume_from_json(json_data)
+            page = research_paper.get_volume_from_json(json_data)
+            research_paper.add_title_json(doi, json_data)
+            row = {}
+            for i in range(len(header)):
+                row[header[i]] = csv_row[i]
+            row["DOI"] = str(doi)
+            row["Authors"] = str_authors
+            row["Year"] = year
+            row["Volume"] = volume
+            row["Page"] = page
+            all_rows.append(row)
+            count += 1
+            print(count)
+    header.append("DOI")
+    header.append("Authors")
+    header.append("Year")
+    header.append("Volume")
+    header.append("Page")
+    write_rows_to_csv(all_rows, output_file, header)
+    research_paper.dump_titles_jsons_file()
+    return
+
+
 if __name__ == "__main__":
     # main()
-    add_doi_json(
+    # add_doi_json(
+    #    DATA_INPUT_DIR + "mailing list-Augmented.csv",
+    #    DATA_OUTPUT_DIR + "mailing list-doi.csv",
+    # )
+    add_doi_authors_year_volume_page(
         DATA_INPUT_DIR + "mailing list-Augmented.csv",
-        DATA_OUTPUT_DIR + "mailing list-doi.csv",
+        DATA_OUTPUT_DIR + "mailing list-doi_authors.csv",
     )
     print("Done!")
